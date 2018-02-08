29bbfac912dd90f82e51008e017aba921b2dc254
amedesc <ametico@gmail.com>
2018-02-07 18:13:19 -0600

    Agregue modelo de bucketlist2

--- api/models.py
+++ api/models.py
@@ -11,3 +11,13 @@ class Bucketlist(models.Model):
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
+
+class Bucketlist2(models.Model):
+    """This class represents the bucketlist model."""
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
